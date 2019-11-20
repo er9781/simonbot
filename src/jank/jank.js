8b20ffe68c93be9be15720bb9d5350f10c93aa45
Shane Schulte <schulte.shane@gmail.com>
2019-11-20 00:42:56 -0800

    fix capture group and add relative jank mode

--- src/jank/jank.js
+++ src/jank/jank.js
@@ -1,25 +1,35 @@
 const got = require("got");
 
-const jankRe = /jank:[0-9]+/;
+const jankRe = /jank:([+\-]?)([0-9]+)/;
 
 const bound = (min, max) => n => Math.max(min, Math.min(max, n));
 const boundJank = bound(0, 100);
-const getJankIndexUpdateFromString = str => {
-  const s = str.match(jankRe)[1];
+
+const modes = {
+  '+': 'add',
+  '-': 'subtract'
+};
+
+const modeString = mode => {
+  if (!mode) return '';
+  return `&mode=${modes[mode]}`;
+}
+const getJankBodyUpdateFromString = str => {
+  const [_, mode, s] = str.match(jankRe);
   if (!s) return undefined;
-  return boundJank(Number(s));
+  return `jank=${boundJank(Number(s))}${modeString(mode)}`;
 }
 
-const postJankIndex = index => got.post(`https://shaneschulte.com/mob_jank_gauge/set_jank.php`, {
-  body: `jank=${index * 10}`,
+const postJankIndex = body => got.post(`https://shaneschulte.com/mob_jank_gauge/set_jank.php`, {
+  body,
   headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
 });
 
 const handlePr = async pr => {
   const text = pr.body;
-  const jankIndex = getJankIndexUpdateFromString(text);
+  const jankBody = getJankBodyUpdateFromString(text);
   if (jank === undefined) return;
-  await postJankIndex(jankIndex);
+  await postJankIndex(jankBody);
 }
 
 exports.postJankIndexFromPr = postJankIndexFromPr;
